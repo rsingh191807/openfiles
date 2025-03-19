6c30a49de589d253804c879ea2f0aba6ad30f0b8
Rishabh Singh <RSingh19@oceaneering.com>
2025-03-19 13:38:23 +0530

    updates

--- src/android/OpenFiles.java
+++ src/android/OpenFiles.java
@@ -1,20 +1,27 @@
 package com.example.openfiles;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
-import android.provider.MediaStore;
+import android.os.Build;
+import android.provider.OpenableColumns;
+import android.database.Cursor;
 import android.util.Base64;
+import android.webkit.MimeTypeMap;
+import androidx.core.content.FileProvider;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class OpenFiles extends CordovaPlugin {
@@ -23,9 +30,10 @@ public class OpenFiles extends CordovaPlugin {
     private CallbackContext callbackContext;
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("open".equals(action)) {
-            this.callbackContext = callbackContext;
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+
+        if (action.equals("open")) {
             openFilePicker();
             return true;
         }
@@ -36,68 +44,76 @@ public class OpenFiles extends CordovaPlugin {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        cordova.setActivityResultCallback(this);
-        cordova.getActivity().startActivityForResult(Intent.createChooser(intent, "Select a file"), PICK_FILE_REQUEST);
+        cordova.startActivityForResult(this, Intent.createChooser(intent, "Select File"), PICK_FILE_REQUEST);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PICK_FILE_REQUEST && resultCode == Activity.RESULT_OK) {
-            if (data != null && data.getData() != null) {
+            if (data != null) {
                 Uri uri = data.getData();
                 try {
-                    // Get file path from URI
-                    String filePath = getPathFromUri(uri);
+                    String fileName = getFileName(uri);
+                    String mimeType = getMimeType(uri);
+                    byte[] fileData = getFileData(uri);
 
-                    if (filePath != null) {
-                        // Read file as binary
-                        byte[] fileData = readFileAsBinary(filePath);
-                        // Convert binary data to Base64
-                        String base64Data = Base64.encodeToString(fileData, Base64.DEFAULT);
+                    JSONObject result = new JSONObject();
+                    result.put("fileName", fileName);
+                    result.put("mimeType", mimeType);
+                    result.put("data", Base64.encodeToString(fileData, Base64.DEFAULT));
 
-                        // Create JSON response
-                        JSONObject result = new JSONObject();
-                        result.put("filePath", filePath);
-                        result.put("fileData", base64Data);
-
-                        // Send response to JavaScript
-                        callbackContext.success(result);
-                    } else {
-                        callbackContext.error("Unable to retrieve file path.");
-                    }
+                    callbackContext.success(result);
                 } catch (Exception e) {
-                    callbackContext.error("Error processing file: " + e.getMessage());
+                    callbackContext.error("Failed to read file: " + e.getMessage());
                 }
             } else {
-                callbackContext.error("No file selected.");
+                callbackContext.error("File selection was canceled.");
             }
         }
     }
 
-    private String getPathFromUri(Uri uri) {
-        String[] projection = { MediaStore.Files.FileColumns.DATA };
-        Cursor cursor = cordova.getActivity().getContentResolver().query(uri, projection, null, null, null);
-        if (cursor != null) {
-            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA);
-            cursor.moveToFirst();
-            String filePath = cursor.getString(columnIndex);
-            cursor.close();
-            return filePath;
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = cordova.getActivity().getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
         }
-        return null;
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 
-    private byte[] readFileAsBinary(String filePath) throws IOException {
-        File file = new File(filePath);
-        FileInputStream fis = new FileInputStream(file);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = fis.read(buffer)) != -1) {
-            bos.write(buffer, 0, bytesRead);
+    private String getMimeType(Uri uri) {
+        String mimeType = null;
+        if (uri.getScheme().equals("content")) {
+            mimeType = cordova.getActivity().getContentResolver().getType(uri);
+        } else {
+            String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
         }
-        fis.close();
-        return bos.toByteArray();
+        return mimeType;
+    }
+
+    private byte[] getFileData(Uri uri) throws IOException {
+        try (InputStream inputStream = cordova.getActivity().getContentResolver().openInputStream(uri);
+             ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream()) {
+
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                byteBuffer.write(buffer, 0, len);
+            }
+            return byteBuffer.toByteArray();
+        }
     }
 }
